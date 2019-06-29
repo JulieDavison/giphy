e349ddf4810ece3af88a9cfe7455e6cffdf7d501
Julie Davison <davison.jules@gmail.com>
2019-06-28 20:05:44 -0700

    added js for 10 gifs and rating

--- disney.js
+++ disney.js
@@ -1,13 +1,23 @@
 // Key
 var APIKey = "ajz5pnllJ9wR0I3K3m5mpK1chLSm4JJe";
 // Character Buttons
-var character = ["Minnie Mouse", "Mickey Mouse", "Goofy", "Bambi", "Happy", "Grumpy", "Tigger", "Piglet", "Ariel", "Sebastian"];
+var character = ["Minnie Mouse", "Mickey Mouse", "Goofy", "Bambi", "Happy", "Peter Pan", "Tigger", "Piglet", "Ariel", "Sebastian"];
+
+makeButton ();
+
+// // Generic function for capturing the movie name from the data-attribute
+// function alertCharacter() {
+//     var characterName = $(this).attr("data-name");
+
+//     alert(characterName);
+// }
+// console.log(this);
 
 function makeButton() {
 
     // Deleting the movies prior to adding new movies
     // (this is necessary otherwise we will have repeat buttons)
-    $("#character-view").empty();
+    $("#newCharacterButton").empty();
 
     // Looping through the array of movies
     for (var i = 0; i < character.length; i++) {
@@ -22,65 +32,84 @@ function makeButton() {
         // Providing the initial button text
         a.text(character[i]);
         // Adding the button to the HTML
-        $("#character-view").append(a);
+        $("#newCharacterButton").append(a);
+        // $("#newCharacterButton").append(a.text.character[i]);
+
+
+
+        // This function handles events where one button is clicked
+        // $(document).on("click", ".character", alertCharacterName);
     }
 
-
-    // This function handles events where one button is clicked
-    $("#add-character").on("click", function (event) {
-        // Preventing the buttons default behavior when clicked (which is submitting a form)
-        event.preventDefault();
-        // This line grabs the input from the textbox
-        var character = $("#character-input").val().trim();
-
-        // Adding the movie from the textbox to our array
-        character.push(character);
-
-        // Calling renderButtons which handles the processing of our movie array
-        makeButton();
+    $(".character").on("click", function
+        () {
+            //This returns the same string that is the text of the button
+            var character = $(this).attr('data-name');
+            var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + APIKey + "&q=" + character + "&rating=G&limit=9";
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+        
+                // After the data from the AJAX request comes back
+                .then(function (response) {
+                    console.log(response);
+                    $('#images').empty();
+                    for (var i = 0; i < response.data.length; i++){
+                        var rating = response.data[i].rating; 
+                        var stillLink = response.data[i].images.fixed_height_still.url;
+                        var gifLink = response.data[i].images.fixed_height.url;
+                        // // Creating and storing an image tag
+                        var container = $('<div>');
+                        var pEl = $('<p>').text(rating);
+                        var disneyImage = $("<img>")
+                                        .attr("src", stillLink)
+                                        .attr("data-still", stillLink)
+                                        .attr("data-gif", gifLink)
+                                        .attr("data-state", 'still')
+                                        .attr("alt", "disney image")
+                                        .addClass('img-click');
+                         
+                        var html = container.append(pEl).append(disneyImage);
+                        // // Prepending the disneyImage to the images div
+                         $("#images").prepend(html);
+                    }
+                    
+               
+        
+        
+                });
 
     });
 }
 
 
 $("#find-character").on("click", function (event) {
-
-    //     // event.preventDefault() can be used to prevent an event's default behavior.
-    //     // Here, it prevents the submit button from trying to submit a form when clicked
     event.preventDefault();
-
-    //     // Here we grab the text from the input box
-    var character = $("#character-input").val();
-
-    //     // Here we construct our URL
-    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=" + APIKey + "&tag=" + character + "&rating=G";
+    var buttonName = $('#character-input').val();
+    character.push(buttonName);
+    makeButton();
+});
 
 
-    // Perfoming an AJAX GET request to our queryURL
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-
-        // After the data from the AJAX request comes back
-        .then(function (response) {
-
-            // Saving the image_original_url property
-            var imageUrl = response.data.image_original_url;
-
-            // Creating and storing an image tag
-            var disneyImage = $("<img>");
-
-            // Setting the disneyImage src attribute to imageUrl
-            disneyImage.attr("src", imageUrl);
-            disneyImage.attr("alt", "disney image");
-
-            // Prepending the disneyImage to the images div
-            $("#images").prepend(disneyImage);
-
-        });
 
 
+    // Pausing gifs
+    $(document).on("click", ".img-click", function () {
+        console.log('i work')
+        // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+        var state = $(this).attr("data-state");
+        // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+        // Then, set the image's data-state to animate
+        // Else set src to the data-still value
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-gif"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    });
 
 
 
@@ -117,8 +146,12 @@ $("#find-character").on("click", function (event) {
 
                 // Prepending the disneyImage to the images div
                 $("#images").prepend(disneyImage);
+                console.log(disneyImage);
 
             });
     });
 
-});
+
+
+
+
